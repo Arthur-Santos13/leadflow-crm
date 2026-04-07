4bdac05d361a9504d3fbf3db4771a49915aa7ce4
Arthur-Santos13 <ahsantos1301@gmail.com>
2026-04-07 12:39:54 -0300

    feat(interactions): delete interaction

--- backend/src/modules/interactions/interactions.routes.ts
+++ backend/src/modules/interactions/interactions.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { authenticate } from '../../middlewares/auth.middleware';
+import { authenticate, authorize } from '../../middlewares/auth.middleware';
 import * as InteractionsController from './interactions.controller';
 
 const router = Router();
@@ -9,6 +9,6 @@ router.use(authenticate);
 router.post('/', InteractionsController.create);
 router.get('/', InteractionsController.list);
 router.get('/:id', InteractionsController.getOne);
-router.delete('/:id', InteractionsController.remove);
+router.delete('/:id', authorize('ADMIN'), InteractionsController.remove);
 
 export default router;
